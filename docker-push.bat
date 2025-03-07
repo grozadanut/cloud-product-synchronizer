ae6a717fe4a1592cb1a0a43d0b75bf510656228c
Groza Danut <grozadanut95@gmail.com>
2025-03-07 11:27:28 +0200

    Fix resttemplate PATCH error

--- docker-push.bat
+++ docker-push.bat
@@ -1,4 +1,4 @@
-set VERSION=0.0.3
+set VERSION=0.0.4
 docker build -t grozadanut/cloud-product-synchronizer:latest -t grozadanut/cloud-product-synchronizer:%VERSION% --target production .
 docker push grozadanut/cloud-product-synchronizer:%VERSION%
 docker push grozadanut/cloud-product-synchronizer:latest
