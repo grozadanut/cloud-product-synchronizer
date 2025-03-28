5feb9c3b0d3c06dce56a6799f99743e4b00e5be5
Groza Danut <grozadanut95@gmail.com>
2025-03-28 13:10:18 +0200

    Add Sentry(GlitchTip) exception logging

--- docker-push.bat
+++ docker-push.bat
@@ -1,4 +1,4 @@
-set VERSION=0.0.4
+set VERSION=0.0.5
 docker build -t grozadanut/cloud-product-synchronizer:latest -t grozadanut/cloud-product-synchronizer:%VERSION% --target production .
 docker push grozadanut/cloud-product-synchronizer:%VERSION%
 docker push grozadanut/cloud-product-synchronizer:latest
